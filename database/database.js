b45b7752e617a228d0a289addb4b1fe114111459
Kleuber Jacob <kleuber.18@hotmail.com>
2021-01-07 16:19:50 -0300

    second commit

--- database/database.js
+++ database/database.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize') // Armazenando a biblioteca de acesso a b
 /* Armazenado em uma variável utilizando a lib todas informacoes que serao utilizadas para acessar o banco 
 de dados nesse caso o MySQL (nome, user, senha) e onde o banco está armazenado
 */
-const connection = new Sequelize('blognodejs', 'kleuber', 'Kleuber201317', {
-    host: 'mysql742.umbler.com',
+const connection = new Sequelize('nome do banco', 'usuario', 'senha', {
+    host: 'endereco servidor',
     dialect: 'mysql',
     timezone: '-03:00' // Configurando o DB para utilizar o meu timezone, pois por padrao ele utiliza um timezone universal diferente do BR
 })
